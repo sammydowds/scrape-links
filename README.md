b6c20ae1a07e3525c01c3f2b056f689c94b76fca
vik <sammydowds1993@gmail.com>
2024-10-25 11:00:57 -0500

    feat: ignore domains, more analysis, remove agent

--- README.md
+++ README.md
@@ -33,3 +33,20 @@ to run
 ```
 poetry run scrapy crawl links
 ```
+
+## Extras
+
+You can also ignore domains with an environment var
+
+```
+// .env
+
+IGNORE_DOMAINS=...
+```
+
+This will prevent common social media domains from being in the output muddying your analysis of unique and more interesting domains.
+
+For example:
+```
+IGNORE_DOMAINS=truthsocial.com,rumble.com,twitter.com,www.facebook.com,www.tiktok.com,www.instagram.com,www.youtube.com,youtu.be,podcasts.apple.com,podcasts.google.com,open.spotify.com
+```
--- links/spiders/link_spider.py
+++ links/spiders/link_spider.py
@@ -2,11 +2,14 @@ from pathlib import Path
 from bs4 import BeautifulSoup, SoupStrainer
 import csv
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 import scrapy
 import os 
 
 load_dotenv()
+
+IGNORE_DOMAINS = os.getenv('IGNORE_DOMAINS')
 
 class LinkSpider(scrapy.Spider):
     name = "links"
@@ -23,25 +26,28 @@ class LinkSpider(scrapy.Spider):
     def parse(self, response):
         soup = BeautifulSoup(response.body, 'html.parser')
         links = soup.find_all("a", href=True)
-        internal_links = []
-        external_links = []
+        internal_routes = []
+        external_domains = []
 
         for link in links:
-            if response.url in link["href"] or link["href"] in response.url:
-                internal_links.append(link["href"])
+            parsedLink = urlparse(link["href"])
+            if parsedLink.netloc in response.url or "https" not in link["href"]:
+                if parsedLink.path and parsedLink.path not in internal_routes:
+                    internal_routes.append(parsedLink.path)
             else:
-                external_links.append(link["href"])
+                if parsedLink.netloc not in IGNORE_DOMAINS and parsedLink.netloc not in external_domains:
+                    external_domains.append(parsedLink.netloc)
 
         # save internal links csv 
         csv_filename = f"./results/{response.url.split('//')[-1].replace('/', '_')}_internal.csv"
         with open(csv_filename, mode='w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
-            for link in internal_links:
-                writer.writerow([link])
+            for routes in internal_routes:
+                writer.writerow([routes])
 
         # save external links csv 
         csv_filename = f"./results/{response.url.split('//')[-1].replace('/', '_')}_external.csv"
         with open(csv_filename, mode='w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
-            for link in external_links:
-                writer.writerow([link])
+            for domain in external_domains:
+                writer.writerow([domain])
